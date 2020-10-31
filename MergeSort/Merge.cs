8ae73dae9f3f9fe7c555ebd4de691b1914d329a9
Ishankr21 <53378786+Ishankr21@users.noreply.github.com>
2020-10-31 20:06:06 +0530

    Update Merge.cs

--- MergeSort/Merge.cs
+++ MergeSort/Merge.cs
@@ -1,97 +1,115 @@
-﻿using System;
-
-namespace MergeSort
+// C# program for Merge Sort
+using System;
+class MergeSort{
+     
+// Merges two subarrays of []arr.
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int []arr, int l, 
+           int m, int r)
 {
-    class Merge
+  // Find sizes of two 
+  // subarrays to be merged
+  int n1 = m - l + 1;
+  int n2 = r - m;
+ 
+  // Create temp arrays
+  int []L = new int[n1];
+  int []R = new int[n2];
+  int i, j;
+   
+  // Copy data to temp arrays
+  for (i = 0; i < n1; ++i)
+    L[i] = arr[l + i];
+  for (j = 0; j < n2; ++j)
+    R[j] = arr[m + 1 + j];
+ 
+  // Merge the temp arrays
+ 
+  // Initial indexes of first 
+  // and second subarrays
+  i = 0;
+  j = 0;
+ 
+  // Initial index of merged 
+  // subarry array
+  int k = l;
+  while (i < n1 && j < n2) 
+  {
+    if (L[i] <= R[j]) 
     {
-        // Merges two subarrays of arr[].
-        // First subarray is arr[left..middle]
-        // Second subarray is arr[middle+1..right]
-        void MergeS(int[] arr, int left, int middle, int right)
-        {
-            // Find sizes of two subarrays to be merged
-            int lenghtFirtArr   = middle - left + 1;
-            int lenghtSecondArr = right  - middle;
-            
-            /* Create temp arrays */
-            int[] tempArrLeft  = new int [lenghtFirtArr];
-            int[] tempArrRight = new int [lenghtSecondArr];
-            
-            /*Copy data to temp arrays*/
-            int i;
-            for (i = 0; i<lenghtFirtArr; ++i)
-                tempArrLeft[i] = arr[left + i];
-
-            int j;
-            for (j = 0; j<lenghtSecondArr; ++j)
-                tempArrRight[j] = arr[middle + 1 + j];
-            
-            /* Merge the temp arrays */
-            // Initial indexes of first and second subarrays
-            i = 0; j = 0;
- 
-            // Initial index of merged subarry array
-            int k = left;
-            while (i < lenghtFirtArr && j < lenghtSecondArr)
-            {
-                if (tempArrLeft[i] <= tempArrRight[j])
-                {
-                    arr[k] = tempArrLeft[i];
-                    i++;
-                }
-                else
-                {
-                    arr[k] = tempArrRight[j];
-                    j++;
-                }
-                k++;
-            }
-            // Copy the remaining elements of tempL[], if there are any 
-            while (i < lenghtFirtArr)
-            { 
-                arr[k] = tempArrLeft[i];
-                i++;
-                k++;
-            }
- 
-            /* Copy the remaining elements of tempR[], if there are any */
-            while (j < lenghtSecondArr)
-            {
-                arr[k] = tempArrRight[j];
-                j++;
-                k++;
-            }
-        }
-        
-        // Main function that sorts arr[left..right] using
-        // merge()
-        void Sort(int[] arr, int left, int right)
-        {
-            if (left < right)
-            {
-                // Find the middle point
-                int middle = (left+right)/2;
- 
-                // Sort first and second halves
-                Sort(arr, left, middle);
-                Sort(arr , middle+1, right);
- 
-                // Merge the sorted halves
-                MergeS(arr, left, middle, right);
-            }
-        }
-        static void Main(string[] args)
-        {
-            int[] array = new int[] {13, 10, 11 ,18, 17, 12, 15, 14, 19, 16};
-            
-            Console.WriteLine("Given Array");
-            Console.WriteLine(String.Join(" ", array));
- 
-            Merge ob = new Merge();
-            ob.Sort(array, 0, array.Length - 1);
- 
-            Console.WriteLine("\nSorted array");
-            Console.WriteLine(String.Join(" ", array));
-        }
+      arr[k] = L[i];
+      i++;
     }
+    else
+    {
+      arr[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+ 
+  // Copy remaining elements 
+  // of L[] if any 
+  while (i < n1) 
+  {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+ 
+  // Copy remaining elements 
+  // of R[] if any 
+  while (j < n2) 
+  {
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
+}
+ 
+// Main function that 
+// sorts arr[l..r] using
+// merge()
+void sort(int []arr, 
+          int l, int r)
+{
+  if (l < r) 
+  {
+    // Find the middle 
+    // point
+    int m = (l + r) / 2;
+ 
+    // Sort first and 
+    // second halves
+    sort(arr, l, m);
+    sort(arr, m + 1, r);
+ 
+    // Merge the sorted halves
+    merge(arr, l, m, r);
+  }
+}
+ 
+// A utility function to 
+// print array of size n */
+static void printArray(int []arr)
+{
+  int n = arr.Length;
+  for (int i = 0; i < n; ++i)
+    Console.Write(arr[i] + " ");
+  Console.WriteLine();
+}
+ 
+// Driver method
+public static void Main(String []args)
+{
+  int []arr = {12, 11, 13, 
+               5, 6, 7};
+  Console.WriteLine("Given Array");
+  printArray(arr);
+  MergeSort ob = new MergeSort();
+  ob.sort(arr, 0, arr.Length - 1);
+  Console.WriteLine("\nSorted array");
+  printArray(arr);
+}
 }
